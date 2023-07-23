98e60020aedcc91fabf99295428ed1aa7b279278
chetan45m <78179892+CHET45@users.noreply.github.com>
2023-07-23 19:35:47 +0300

    Delete 06_2023 directory

--- 06_2023/Enemies/Red_monster.gd
+++
@@ -1,2 +0,0 @@
-extends "res://Enemies/Enemies.gd"
-
--- 06_2023/Player/Man_anmation.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-@export var motion_flag:bool
-var frame_pos:int=5
-func Man_animation():
-	frame=frame_pos	
-	frame_pos+=1
-	$Timer.start()
-	if frame_pos>8:
-		frame_pos=5
