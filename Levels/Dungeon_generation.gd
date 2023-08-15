1d07f59a6af31b6892e38c7eee295160001879b8
chetan45m <78179892+CHET45@users.noreply.github.com>
2023-08-16 01:38:23 +0300

    Add files via upload

--- Levels/Dungeon_generation.gd
+++ Levels/Dungeon_generation.gd
@@ -5,11 +5,12 @@ extends Node2D
 @export var rooms_size = Vector2(15, 25)
 @export var rooms_max = 10
 @export var corridor_size=3
-var data = {}
+var data={}
 var enemies={}
-var rooms=[]
-var used_rooms={}
+var rooms={}
+var sweeped_rooms={}
 var dors=[]
+var rendering_mode="standart"
 @onready var level: TileMap = $Level
 
 func _generate():
@@ -25,36 +26,63 @@ func _generate():
 		enemies={}
 		for room in rooms:
 			remove_child(room)
-		rooms = []
-		used_rooms={}
+		rooms = {}
+		sweeped_rooms={}
 		dors=[]
 		regenerate=_generate_data(rng)
-	_reduce_room_area()
+	_room_area_and_walls()
+	await get_tree().physics_frame
 	_add_connections(rng)
+	await get_tree().physics_frame
 	_add_dors()
-	_fill_level()
+	await get_tree().physics_frame
+	_room_cells()
 	
-func _fill_level():
-	for point in data:
-		if data.get(point)==1 or data.get(point)==2:
-			level.set_cell(0,point,0,Vector2(12,4))
-		elif data.get(Vector2(point))==3:
-			if data.get(point+Vector2(0,1))!=3:
-				level.set_cell(1,point+Vector2(0,1),0,Vector2(24,6))
-			level.set_cell(0,point,0,Vector2(12,4))
+func _fill_level(room:Area2D):
+	if rendering_mode=="standart":
+		for point in data:
+			if $Player.position.distance_squared_to(point*32)<470000:
+				if data.get(point)==1 or data.get(point)==2:
+					level.set_cell(0,point,0,Vector2(12,4))
+				elif data.get(Vector2(point))==3:
+					if data.get(point+Vector2(0,1))!=3:
+						level.set_cell(1,point+Vector2(0,1),0,Vector2(24,6))
+					level.set_cell(0,point,0,Vector2(12,4))
+				elif data.get(Vector2(point))==0:
+					level.set_cell(0,point,0,Vector2(38,6))
+			else:
+				level.erase_cell(0,point)
+				level.erase_cell(1,point)
+	elif rendering_mode=="fight":
+		level.clear()
+		for point in rooms.get(room):
+			if data.get(point) in [1,2,3]:
+				level.set_cell(0,point,0,Vector2(12,4))
+			elif data.get(Vector2(point))==0: 
+				level.set_cell(0,point,0,Vector2(38,6))
+	elif rendering_mode=="debug":
+		for point in data:
+			if data.get(point)==1 or data.get(point)==2:
+				level.set_cell(0,point,0,Vector2(12,4))
+			elif data.get(Vector2(point))==3:
+				if data.get(point+Vector2(0,1))!=3:
+					level.set_cell(1,point+Vector2(0,1),0,Vector2(24,6))
+				level.set_cell(0,point,0,Vector2(12,4))
+			elif data.get(Vector2(point))==0:
+				level.set_cell(0,point,0,Vector2(38,6))
 
 func _generate_data(rng:RandomNumberGenerator):
 	rng.randomize()
-	var room:Area2D
+	var room
 	var tries_before_regeneration=20
 	while rooms.size()<7 and rooms.size()<rooms_max:
 		room = _get_random_room(rng)
 		if room:
 			tries_before_regeneration=20
-			rooms.push_back(room)
-			add_child(room)
-			room.body_entered.connect(_player_enters_room)
-			_add_creatures(rng,room)
+			rooms[room[0]]=room[1]
+			add_child(room[0])
+			room[0].body_entered.connect(_player_enters_room)
+			_add_creatures(rng,room[0])
 		elif tries_before_regeneration<=0 and data.size()>7000:
 			return true
 		elif tries_before_regeneration<=-30:
@@ -106,39 +134,122 @@ func _get_random_room(rng: RandomNumberGenerator) :
 	col.set_shape(shape)
 	area.position=Vector2(posx,posy)*c
 	data.merge(room_dic)
-	return area
+	var r=[area,room_dic.keys()]
+	return r
 
-func _reduce_room_area():
+func _room_area_and_walls():
 	for room in rooms:
-		var posx=room.position.x/32
-		var posy=room.position.y/32
 		if room.get_child(0).get_shape().get_class()=="CircleShape2D":
 			var radius=room.get_child(0).get_shape().get_radius()/32
-			room.get_child(0).get_shape().set_radius((radius-3.5)*32)
-			for xi in range(-radius,radius+1):
-				var y=round(sqrt(-xi*xi+radius*radius))
-				for yi in range(-y,-y+3):
-					data.erase(Vector2(xi+posx,yi+posy))
-				for yi in range(y-2,y+1):
-					data.erase(Vector2(xi+posx,yi+posy))
-			for yi in range(-radius,radius+1):
-				var x=round(sqrt(-yi*yi+radius*radius))
-				for xi in range(-x,-x+3):
-					data.erase(Vector2(xi+posx,yi+posy))
-				for xi in range(x-2,x+1):
-					data.erase(Vector2(xi+posx,yi+posy))
+			room.get_child(0).get_shape().set_radius((radius-2.5)*32)
 		elif room.get_child(0).get_shape().get_class()=="RectangleShape2D":
 			var width=room.get_child(0).get_shape().get_size().x/64
 			var height=room.get_child(0).get_shape().get_size().y/64
-			room.get_child(0).get_shape().set_size(Vector2(width-4,height-4)*64)
-			for w in 3:
-				for xi in range(posx-width, posx+width):
-					data[Vector2(xi,posy+height-w)]=null
-					data[Vector2(xi,posy-height+w)]=null
-				for yi in range(posy-height, posy+height):
-					data[Vector2(posx+width-w, yi)]=null
-					data[Vector2(posx-width+w, yi)]=null	
+			room.get_child(0).get_shape().set_size(Vector2(width-2,height-2)*64)
+		for point in rooms.get(room):
+			if !rooms.get(room).has(point+Vector2(-1,0)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(-1,-1)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(0,-1)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(1,-1)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(1,0)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(1,1)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(0,1)):
+				data[point]=0
+				continue
+			if !rooms.get(room).has(point+Vector2(-1,1)):
+				data[point]=0
+				continue
+		
 	print("2")
+
+func _room_cells():
+	for room in rooms:
+		for point in rooms.get(room):
+			if data.get(point)==1:
+				var corridor_cells=[point]
+				var index=0
+				var point_count=1
+				while index<point_count:
+					point=corridor_cells[index]
+					if (data.get(point+Vector2(-1,0))==2 and !corridor_cells.has(point+Vector2(-1,0)) or
+					  data.get(point+Vector2(-1,0))==1 and !rooms.get(room).has(point+Vector2(-1,0))):
+						corridor_cells.push_back(point+Vector2(-1,0))
+						rooms.get(room).push_back(point+Vector2(-1,0))
+						point_count+=1
+					elif data.get(point+Vector2(-1,0))==3 or data.get(point+Vector2(-1,0))==0:
+						if !rooms.get(room).has(point+Vector2(-1,0)):
+							rooms.get(room).push_back(point+Vector2(-1,0))
+					if (data.get(point+Vector2(-1,-1))==2 and !corridor_cells.has(point+Vector2(-1,-1)) or
+					  data.get(point+Vector2(-1,-1))==1 and !rooms.get(room).has(point+Vector2(-1,-1))):
+						corridor_cells.push_back(point+Vector2(-1,-1))
+						rooms.get(room).push_back(point+Vector2(-1,-1))
+						point_count+=1
+					elif data.get(point+Vector2(-1,-1))==3 or data.get(point+Vector2(-1,-1))==0:
+						if !rooms.get(room).has(point+Vector2(-1,-1)):
+							rooms.get(room).push_back(point+Vector2(-1,-1))
+					if (data.get(point+Vector2(0,-1))==2 and !corridor_cells.has(point+Vector2(0,-1)) or
+					  data.get(point+Vector2(0,-1))==1 and !rooms.get(room).has(point+Vector2(0,-1))):
+						corridor_cells.push_back(point+Vector2(0,-1))
+						rooms.get(room).push_back(point+Vector2(0,-1))
+						point_count+=1
+					elif data.get(point+Vector2(0,-1))==3 or data.get(point+Vector2(0,-1))==0:
+						if !rooms.get(room).has(point+Vector2(0,-1)):
+							rooms.get(room).push_back(point+Vector2(0,-1))
+					if (data.get(point+Vector2(1,-1))==2 and !corridor_cells.has(point+Vector2(1,-1)) or
+					  data.get(point+Vector2(1,-1))==1 and !rooms.get(room).has(point+Vector2(1,-1))):
+						corridor_cells.push_back(point+Vector2(1,-1))
+						rooms.get(room).push_back(point+Vector2(1,-1))
+						point_count+=1
+					elif data.get(point+Vector2(1,-1))==3 or data.get(point+Vector2(1,-1))==0:
+						if !rooms.get(room).has(point+Vector2(1,-1)):
+							rooms.get(room).push_back(point+Vector2(1,-1))
+					if (data.get(point+Vector2(1,0))==2 and !corridor_cells.has(point+Vector2(1,0)) or
+					  data.get(point+Vector2(1,0))==1 and !rooms.get(room).has(point+Vector2(1,0))):
+						corridor_cells.push_back(point+Vector2(1,0))
+						rooms.get(room).push_back(point+Vector2(1,0))
+						point_count+=1
+					elif data.get(point+Vector2(1,0))==3 or data.get(point+Vector2(1,0))==0:
+						if !rooms.get(room).has(point+Vector2(1,0)):
+							rooms.get(room).push_back(point+Vector2(1,0))
+					if (data.get(point+Vector2(1,1))==2 and !corridor_cells.has(point+Vector2(1,1)) or
+					  data.get(point+Vector2(1,1))==1 and !rooms.get(room).has(point+Vector2(1,1))):
+						corridor_cells.push_back(point+Vector2(1,1))
+						rooms.get(room).push_back(point+Vector2(1,1))
+						point_count+=1
+					elif data.get(point+Vector2(1,1))==3 or data.get(point+Vector2(1,1))==0:
+						if !rooms.get(room).has(point+Vector2(1,1)):
+							rooms.get(room).push_back(point+Vector2(1,1))
+					if (data.get(point+Vector2(0,1))==2 and !corridor_cells.has(point+Vector2(0,1)) or
+					  data.get(point+Vector2(0,1))==1 and !rooms.get(room).has(point+Vector2(0,1))):
+						corridor_cells.push_back(point+Vector2(0,1))
+						rooms.get(room).push_back(point+Vector2(0,1))
+						point_count+=1
+					elif data.get(point+Vector2(0,1))==3 or data.get(point+Vector2(0,1))==0:
+						if !rooms.get(room).has(point+Vector2(0,1)):
+							rooms.get(room).push_back(point+Vector2(0,1))
+					if (data.get(point+Vector2(-1,1))==2 and !corridor_cells.has(point+Vector2(-1,1)) or
+					  data.get(point+Vector2(-1,1))==1 and !rooms.get(room).has(point+Vector2(-1,1))):
+						corridor_cells.push_back(point+Vector2(-1,1))
+						rooms.get(room).push_back(point+Vector2(-1,1))
+						point_count+=1
+					elif data.get(point+Vector2(-1,1))==3 or data.get(point+Vector2(-1,1))==0:
+						if !rooms.get(room).has(point+Vector2(-1,1)):
+							rooms.get(room).push_back(point+Vector2(-1,1))
+					index+=1
+	print("6")
+
 
 func _add_dors():
 	var all_dors={}
@@ -153,49 +264,49 @@ func _add_all_dors(all_dors:Dictionary):
 			if data.get(Vector2(x,y))==2:
 				var maybedor=[]
 				var hororver#dor is horizontal or vertical
-				if data.get(Vector2(x-1,y))==null:
+				if data.get(Vector2(x-1,y))==0:
 					for cell in range(corridor_size*2+1):
 						if data.get(Vector2(x+cell,y))==2 and cell>=corridor_size*2:
 							maybedor=[]
 						elif data.get(Vector2(x+cell,y))==2:
 							maybedor.push_back(Vector2(x+cell,y))
-						elif data.get(Vector2(x+cell,y))==null:
+						elif data.get(Vector2(x+cell,y))==0:
 							break
 						else:
 							maybedor=[]
 							break
 						hororver=0
-				elif data.get(Vector2(x,y-1))==null:
+				elif data.get(Vector2(x,y-1))==0:
 					for cell in range(corridor_size*2+1):
 						if data.get(Vector2(x,y+cell))==2 and cell>=corridor_size*2:
 							maybedor=[]
 						elif data.get(Vector2(x,y+cell))==2:
 							maybedor.push_back(Vector2(x,y+cell))
-						elif data.get(Vector2(x,y+cell))==null:
+						elif data.get(Vector2(x,y+cell))==0:
 							break
 						else:
 							maybedor=[]
 							break
 						hororver=1
-				elif data.get(Vector2(x+1,y))==null:
+				elif data.get(Vector2(x+1,y))==0:
 					for cell in range(corridor_size*2+1):
 						if data.get(Vector2(x-cell,y))==2 and cell>=corridor_size*2:
 							maybedor=[]
 						elif data.get(Vector2(x-cell,y))==2:
 							maybedor.push_back(Vector2(x-cell,y))
-						elif data.get(Vector2(x-cell,y))==null:
+						elif data.get(Vector2(x-cell,y))==0:
 							break
 						else:
 							maybedor=[]
 							break
 						hororver=0
-				elif data.get(Vector2(x,y+1))==null:
+				elif data.get(Vector2(x,y+1))==0:
 					for cell in range(corridor_size*2+1):
 						if data.get(Vector2(x,y-cell))==2 and cell>=corridor_size*2:
 							maybedor=[]
 						elif data.get(Vector2(x,y-cell))==2:
 							maybedor.push_back(Vector2(x,y-cell))
-						elif data.get(Vector2(x,y-cell))==null:
+						elif data.get(Vector2(x,y-cell))==0:
 							break
 						else:
 							maybedor=[]
@@ -207,8 +318,6 @@ func _add_all_dors(all_dors:Dictionary):
 						data[point]=3
 						linedor.add_point(point)
 					all_dors[linedor]=hororver
-			elif data.get(Vector2(x,y))==null:
-				level.set_cell(0,Vector2(x,y),0,Vector2(36,6))
 
 func _choose_best_dors(all_dors:Dictionary):
 	for dor in all_dors:
@@ -287,25 +396,23 @@ func _choose_best_dors(all_dors:Dictionary):
 		#	dors.push_back(dor)
 	all_dors.clear()
 
-func _change_dors_mode(Open:bool,room_center:Vector2):
-	for dor in dors:
-		if (dor.get_point_position(0)*32).distance_squared_to(room_center)<rooms_size.y*32*1.3*rooms_size.y*32*1.3*1.2:
-			for pt in dor.get_point_count():
-				if Open:
-					if data.get(dor.get_point_position(pt)-Vector2(0,1))!=3:
-						level.set_cell(1,dor.get_point_position(pt)-Vector2(0,1),0,Vector2(2,5))
-					if data.get(dor.get_point_position(pt)+Vector2(0,1))!=3:
-						level.set_cell(1,dor.get_point_position(pt)+Vector2(0,1),0,Vector2(24,6))
-					level.set_cell(0,dor.get_point_position(pt),0,Vector2(12,4))
-					level.set_cell(1,dor.get_point_position(pt),0,Vector2(2,5))
-				else:
-					if data.get(dor.get_point_position(pt)-Vector2(0,1))!=3:
-						level.set_cell(1,dor.get_point_position(pt)-Vector2(0,1),0,Vector2(46,6))
-					if data.get(dor.get_point_position(pt)+Vector2(0,1))!=3:
-						level.set_cell(1,dor.get_point_position(pt)+Vector2(0,1),0,Vector2(50,4))
-					level.set_cell(1,dor.get_point_position(pt),0,Vector2(30,4))
-		else:
-			continue
+func _change_dors_mode(Open:bool,room):
+	for point in rooms.get(room):
+		if data.get(point)==3:
+			if Open:
+				if data.get(point-Vector2(0,1))!=3:
+					level.erase_cell(1,point-Vector2(0,1))
+				if data.get(point+Vector2(0,1))!=3:
+					level.set_cell(1,point+Vector2(0,1),0,Vector2(24,6))
+				level.set_cell(0,point,0,Vector2(12,4))
+				level.erase_cell(1,point)
+			else:
+				if data.get(point-Vector2(0,1))!=3:
+					level.set_cell(1,point-Vector2(0,1),0,Vector2(46,6))
+				if data.get(point+Vector2(0,1))!=3:
+					level.set_cell(1,point+Vector2(0,1),0,Vector2(50,4))
+				level.set_cell(1,point,0,Vector2(30,4))
+
 
 func _add_creatures(rng:RandomNumberGenerator, room:Area2D):
 	if $Player.position!=Vector2(0,0):
@@ -319,19 +426,20 @@ func _add_creatures(rng:RandomNumberGenerator, room:Area2D):
 			child_enemy.room=room
 		enemies[room]=room_monsters
 	else:
-		used_rooms[room]=true
+		sweeped_rooms[room]=true
 		$Player.position=room.position
 
 func _add_connections(rng: RandomNumberGenerator):
-	var k=1
+	var used_rooms=[]
 	for room in rooms:
+		if !used_rooms.has(room):
+			used_rooms.push_back(room)
 		var distace=9999999
 		var nearest_room:Area2D
-		for room2_index in range(k, rooms.size()):
-			if distace>(room.position/32).distance_squared_to(rooms[room2_index].position/32):
-				distace=(room.position/32).distance_squared_to(rooms[room2_index].position/32)
-				nearest_room=rooms[room2_index]
-		k+=1
+		for room2 in rooms:
+			if distace>(room.position/32).distance_squared_to(room2.position/32) and !used_rooms.has(room2):
+				distace=(room.position/32).distance_squared_to(room2.position/32)
+				nearest_room=room2
 		if nearest_room:
 			var room_center1 = room.position/32
 			var room_center2 = nearest_room.position/32
@@ -342,24 +450,53 @@ func _add_connections(rng: RandomNumberGenerator):
 				_add_corridor(room_center1.y, room_center2.y, room_center1.x, Vector2.AXIS_Y)#Exit vertical
 				_add_corridor(room_center1.x, room_center2.x, room_center2.y, Vector2.AXIS_X)#Enter horizontal
 	print("3")
+	_add_walls_for_corridors()
+	print("3.5")
 
 func _add_corridor( start: int, end: int, constant: int, axis: int) :
-	for cor_length in corridor_size:
+	for cor_length in corridor_size+2:
 		for t in range(min(start, end), max(start, end) + cor_length+1):
 			var point = Vector2.ZERO
 			match axis:
 				Vector2.AXIS_X: point = Vector2(t, constant+cor_length)
 				Vector2.AXIS_Y: point = Vector2(constant+cor_length, t)
 			if data.get(point)!=1:
-				data[point] = 2
+				if cor_length>0 and cor_length<corridor_size+1:
+					data[point] = 2
+				elif  data.get(point)==null:
+					data[point]=0
+
+func _add_walls_for_corridors():
+	for point in data:
+		if data.get(point)==2:
+			if data.get(point+Vector2(-1,0))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(-1,-1))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(0,-1))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(1,-1))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(1,0))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(1,1))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(0,1))==null:
+				data[point]=0
+				continue
+			if data.get(point+Vector2(-1,1))==null:
+				data[point]=0
+				continue
 
 func _player_enters_room(_body:Node2D):
 	pass
 func find_nearest_room(player):
-	var distance=level_size.x*level_size.y*level_size.x*level_size.y
-	var nearest_room:Area2D
 	for room in rooms:
-		if room.position.distance_squared_to(player)<distance:
-			distance=room.position.distance_squared_to(player)
-			nearest_room=room
-	return nearest_room
+		if rooms.get(room).has(Vector2(level.local_to_map(player))):
+			return room
